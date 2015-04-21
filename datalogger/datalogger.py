4bf351146704f0ab228d89d1dce163c8e4292779
Vance B <vance@axxe.co.uk>
2015-04-21 16:09:08 +0100

    Updated to limit the number of decimal places in the battery readings

--- datalogger/datalogger.py
+++ datalogger/datalogger.py
@@ -221,8 +221,8 @@ class weatherHandler(msgHandler):
         msg["outT"] = values[5]/100.0
         msg["humidity"] = values[6]/100.0
         msg["light"] = values[7]
-        msg["battery"] = values[8]/155.0
-        msg["solar"] = values[9]/155.0
+        msg["battery"] = (values[8]*64)/10000.0 # Should give 5 decimal places
+        msg["solar"] = (values[9]*64)/10000.0 # Should give 5 decimal places
 
         return self.createFields(msg)
 
